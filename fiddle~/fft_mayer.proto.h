dbebd29b7e88573f0ddc5371bd6db6a676163a67
v7b1 <vboehm@gmx.ch>
2015-12-10 13:36:31 +0100

    make a basic 64 bit version

---
+++ fiddle~/fft_mayer.proto.h
@@ -0,0 +1,1 @@
+void fht(float *fz, int n);void ifft(int n, float *real, float *imag);void realfft(int n, float *real);void fft(int n, float *real, float *imag);void realifft(int n, float *real);
