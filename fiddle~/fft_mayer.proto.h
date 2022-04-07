d02b7ceb01ed4b0c387145610155c48f62735680
v7b1 <vboehm@gmx.ch>
2022-04-07 21:09:13 +0200

    initial setup for new package structure

--- fiddle~/fft_mayer.proto.h
+++
@@ -1,1 +0,0 @@
-void fht(float *fz, int n);void ifft(int n, float *real, float *imag);void realfft(int n, float *real);void fft(int n, float *real, float *imag);void realifft(int n, float *real);
